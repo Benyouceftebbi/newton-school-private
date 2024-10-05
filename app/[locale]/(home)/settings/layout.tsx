d9b355515fb7bf2374e18ba3ef17a3a5d98f4130
Salah SAID <105821405+SalahDvp@users.noreply.github.com>
2024-10-05 16:55:26 +0100

    Update layout.tsx

--- app/[locale]/(home)/settings/layout.tsx
+++ app/[locale]/(home)/settings/layout.tsx
@@ -40,7 +40,7 @@ export default function SettingsLayout({ children , params: {locale}}: SettingsL
           <aside className="-mx-4 lg:w-1/5">
             <SidebarNav items={sidebarNavItems} />
           </aside>
-          <div className="flex-1 lg:max-w-2xl">{children}</div>
+          <div className="flex-1 w-full">{children}</div>
         </div>
       </div>
 
